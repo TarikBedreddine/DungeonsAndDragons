fdfbca270aabf9456c56b6da5e730df2e0e98658
Tarik <tarikbedreddine75@gmail.com>
2021-04-28 19:21:31 +0200

    feat: attributes for character classes done fixes #4

--- src/character/Magician.java
+++ src/character/Magician.java
@@ -1,4 +1,10 @@
 package character;
 
 public class Magician {
+    private String name;
+    private String image;
+    private int life;
+    private int attack;
+    private boolean spell = false;
+    private boolean philtre = false;
 }
--- src/character/Warrior.java
+++ src/character/Warrior.java
@@ -1,4 +1,15 @@
 package character;
 
 public class Warrior {
+
+    // Attributes of a Warrior
+    private String name;
+    private String image;
+    private int life;
+    private int attack;
+    private boolean weapon = false;
+    private boolean sheild = false;
+
+
+
 }
