3543a246ee617ba6319ada0a605b32874b8ba6a0
TarikBed <tarik.bedreddine@le-campus-numerique.fr>
2021-04-29 14:25:26 +0200

    feat: App class created & main moved #10

--- src/menu/Navigation.java
+++ src/menu/Navigation.java
@@ -23,17 +23,4 @@ public class Navigation
         return allChoices;
     }
 
-
-
-    // Main entry point of the project
-    public static void main (String[] args ) {
-        // instance of the navigation class to use the userChoices method
-        Navigation userChoices = new Navigation();
-        // Store in an array the retrun of userChoices method
-        String[] allUserChoices = userChoices.userChoices();
-        // Instance of CreateCharacter class constructor
-        CreateCharacter character = new CreateCharacter();
-        // Method of the CreateCharacter class that permit to retrieve all attributes of the character
-        character.createAndDisplayCharacter(allUserChoices[0], allUserChoices[1]);
-    }
 }
