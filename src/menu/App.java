c581816055261a02e32c0c6ed898ace14ca67e5a
TarikBed <tarik.bedreddine@le-campus-numerique.fr>
2021-04-29 15:31:26 +0200

    feat: method to modify the name of a character OK!

--- src/menu/App.java
+++ src/menu/App.java
@@ -7,16 +7,20 @@ public class App {
 
     }
 
+
     // Main entry point of the project
     public static void main (String[] args ) {
         // instance of the navigation class to use the userChoices method
         Navigation userChoices = new Navigation();
         // Store in an array the return of userChoices method
         String[] allUserChoices = userChoices.userChoices();
+        String selectedCharacter = allUserChoices[0];
         // Instance of CreateCharacter class constructor
         CreateCharacter character = new CreateCharacter();
         // Method of the CreateCharacter class that permit to retrieve all attributes of the character
         character.createAndDisplayCharacter(allUserChoices[0], allUserChoices[1]);
+        // Offer the possibility to the user to change the name of the character
+        userChoices.modifyNameChoice(selectedCharacter);
     }
 }
 
--- src/menu/Navigation.java
+++ src/menu/Navigation.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class Navigation
 {
+    // All attributes
+    private String character;
+
     // This method will get the character & name chosen by the user. The return is an String array
     public String[] userChoices () {
         String[] allChoices = new String[2];
@@ -18,9 +21,21 @@ public class Navigation
             allChoices[0] = scanner.nextLine();
         }
         System.out.println("Quel nom voulez-vous lui donner ?");
-        // Store the value of the name at the index 1 of the allChoices array
         allChoices[1] = scanner.nextLine();
         return allChoices;
     }
 
+    public void modifyNameChoice (String character) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Voulez-vous changer le nom de votre personnage ? (Oui ou Non)");
+        String userAnswer = scanner.nextLine();
+        if(userAnswer.equalsIgnoreCase("Oui")) {
+            System.out.println("Veuillez saisir le nouveau nom !");
+            String newName = scanner.nextLine();
+            CreateCharacter applyModification = new CreateCharacter();
+            applyModification.createAndDisplayCharacter(character, newName);
+        } else {
+            System.out.println("C'est noté ! le nom de votre personnage reste inchangé");
+        }
+    }
 }
