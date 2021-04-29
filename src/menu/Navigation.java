0b4e9caa0751832d13f528dd5ec6277d34a7efcb
TarikBed <tarik.bedreddine@le-campus-numerique.fr>
2021-04-29 16:30:26 +0200

    fix: implementation of the method to allow the user to leave the game fixes #12

--- src/menu/Navigation.java
+++ src/menu/Navigation.java
@@ -6,6 +6,7 @@ public class Navigation
 {
     // All attributes
     private String character;
+    Scanner scanner = new Scanner(System.in);
 
     // Set the value of character (Warrior or magician)
     public void setCharacter(String whichCharacter) {
@@ -16,6 +17,27 @@ public class Navigation
         return character;
     }
 
+    // This method permit to execute all the methods of the navigation class
+    public void startMenu() {
+        // Ask the user to start or end the game
+        System.out.println("1 - Commencer le jeu || 2 - Quitter le jeu (Veuillez saisir 1 ou 2)" );
+        int userAnswer = scanner.nextInt();
+        // While the user doesn't leave the game, he can create an other character
+        while (userAnswer == (1)) {
+            System.out.println("--------------------------------------------------------------" );
+            System.out.println("Nous allons commencer la création d'un nouveau personnage !" );
+            // Store in an array the return of userChoices method
+            String[] allUserChoices = userChoices();
+            // Instance of CreateCharacter class constructor
+            CreateCharacter character = new CreateCharacter();
+            // Method of the CreateCharacter class that permit to retrieve all attributes of the character
+            character.createAndDisplayCharacter(allUserChoices[0], allUserChoices[1]);
+            // Offer the possibility for the user to change the name of the character
+            modifyNameChoice();
+        }
+        // method to end the game
+        leaveGame();
+    }
     // This method will get the character & name chosen by the user. The return is an String array
     public String[] userChoices () {
         String[] allChoices = new String[2];
@@ -36,13 +58,14 @@ public class Navigation
         return allChoices;
     }
 
+    // This method permit to modify the name of the character
     public void modifyNameChoice () {
         String character = getCharacter();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Voulez-vous changer le nom de votre personnage ? (Oui ou Non)");
         String userAnswer = scanner.nextLine();
         while (!userAnswer.equalsIgnoreCase("Oui") && !userAnswer.equalsIgnoreCase("Non")) {
-            System.out.println("Voulez-vous changer le nom de votre personnage ? (Oui ou Non)");
+            System.out.println("Veuillez répondre par OUI ou par NON s'il vous plaît ?");
             userAnswer = scanner.nextLine();
         }
         if(userAnswer.equalsIgnoreCase("Oui")) {
@@ -52,6 +75,24 @@ public class Navigation
             applyModification.createAndDisplayCharacter(character, newName);
         } else {
             System.out.println("C'est noté ! le nom de votre personnage reste inchangé");
+        }
+    }
+
+    // This method permit to the use to end the game
+    public void leaveGame() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Etes vous sûr de vouloir quitter ? (Oui ou Non)");
+        String userAnswer = scanner.nextLine();
+        while (!userAnswer.equalsIgnoreCase("Oui") && !userAnswer.equalsIgnoreCase("Non")) {
+            System.out.println("Répondez par OUI ou NON s'il vous plaît ?");
+            userAnswer = scanner.nextLine();
+        }
+        if(userAnswer.equalsIgnoreCase("Oui")) {
+            System.out.println("Merci de votre passage, au revoir !");
+            System.exit(0);
+
+        } else {
+            System.out.println("Super ! poursuivons notre aventure !");
         }
     }
 }
