2c80eca6bffbf3fd4d59d0287b96858781289ec6
Tarik <tarikbedreddine75@gmail.com>
2021-05-02 14:09:46 +0200

    fix: add a while loop to check if a character is created

--- src/game/Game.java
+++ src/game/Game.java
@@ -3,6 +3,8 @@ package game;
 import character.Character;
 import menu.Navigation;
 
+import java.util.Scanner;
+
 public class Game {
 
     // All attributes
@@ -10,21 +12,42 @@ public class Game {
     private Dice dice;
     private BoardGame boardGame;
     private Navigation navigation;
+    private Scanner scanner;
 
     // Constructor of Game Class. Instance all Classes needed
     public Game() {
         this.character = null;
         this.navigation = new Navigation();
         this.dice = new Dice();
+        this.scanner = new Scanner(System.in);
     }
 
     // This method will start the game
     public void runGame() {
-        character = navigation.startMenu(character);
+        while (character == null) {
+            character = navigation.startMenu(character);
+        }
         this.boardGame = new BoardGame(this.character);
+        System.out.println("----------------------------------------------------------------------");
         System.out.println("--------------- Le jeu va débuter, êtes-vous prêts ? -----------------");
+        System.out.println("----------------------------------------------------------------------");
+
         // Initialize box to 1 of the boardGame
         boardGame.initializeFirstBox();
-        boardGame.currentBox();
+        while (character.getCharacterPosition() < 64) {
+            System.out.println("");
+            System.out.println("Appuyer sur la touche \"Entrée\" pour lancer le dé");
+            System.out.println("--------------------------------------------------");
+            String isEnterPressed = scanner.nextLine();
+            if (isEnterPressed.equals("")) {
+                // Throw the dice and set the new position
+                boardGame.currentBox(dice.throwDice());
+            } else {
+                System.out.println("Attention : Seule la touche Entrée permet de lancer le dé ;)");
+            }
+        }
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("--------------- Félicitations vous avez fini le Jeu ------------------");
+        System.out.println("----------------------------------------------------------------------");
     }
 }
