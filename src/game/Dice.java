0110e80901bd3c10a406aa1ebe15dfc26074841c
Tarik <tarikbedreddine75@gmail.com>
2021-05-02 14:07:28 +0200

    feat: implement Dice class with the method to throw the dice fixes #17

--- src/game/Dice.java
+++ src/game/Dice.java
@@ -1,4 +1,26 @@
 package game;
 
 public class Dice {
+
+    // Attributes
+    private int diceValue;
+
+    // Constructors
+    public Dice() {
+        this.diceValue = 0;
+    }
+
+    // Methods
+    public int throwDice (){
+        int range = (6 - 1) + 1;
+        this.diceValue = (int)(Math.random() * range) + 1;
+        return diceValue;
+    }
+    //Getters & Setters
+    public int getDiceValue() {
+        return diceValue;
+    }
+    public void setDiceValue(int diceValue) {
+        this.diceValue = diceValue;
+    }
 }
