c9e1c95e2fd4be25f7819e9d68ab76a47d3e0bc3
TarikBed <tarik.bedreddine@le-campus-numerique.fr>
2021-05-03 13:26:45 +0200

    feat: weapon class w/ getters & setters

---
+++ src/character/Weapon.java
@@ -0,0 +1,33 @@
+package character;
+
+public class Weapon {
+    // Attributes
+    private String name;
+    private int strenght;
+
+    // Constructor
+    public Weapon(String weaponName, int weaponStrenght) {
+        this.name = weaponName;
+        this.strenght = weaponStrenght;
+    }
+
+    // Methods
+
+    // Getters & Setters 
+
+    public int getStrenght() {
+        return strenght;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setStrenght(int strenght) {
+        this.strenght = strenght;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
