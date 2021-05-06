0aa62979b0ff07ee1270ef11268c3f613f0bc8ac
TarikBed <tarik.bedreddine@le-campus-numerique.fr>
2021-05-06 15:54:52 +0200

    feat: refactor add item method with an enum list

--- src/game/boardGame/BoardGame.java
+++ src/game/boardGame/BoardGame.java
@@ -3,18 +3,16 @@ package game.boardGame;
 import character.Character;
 import game.boardGame.cell.*;
 import game.boardGame.cell.enemyCell.Dragon;
-import game.boardGame.cell.enemyCell.Gobelin;
+import game.boardGame.cell.enemyCell.Goblin;
 import game.boardGame.cell.enemyCell.Sorcerer;
 import game.boardGame.cell.shieldCell.BigPotion;
 import game.boardGame.cell.shieldCell.StandardPotion;
-import game.boardGame.cell.surpriseCell.SurpriseCell;
 import game.boardGame.cell.weapon.Club;
 import game.boardGame.cell.weapon.Fireball;
 import game.boardGame.cell.weapon.Sword;
 import game.boardGame.cell.weapon.Thunderbolt;
 
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,99 +22,105 @@ public class BoardGame {
     private int numberOfBoxes;
     private List<Cell> boardGame;
     private Character character;
-    private int whichArray;
 
     // Constructors
     public BoardGame(Character character) {
         this.numberOfBoxes = 64;
         this.character = character;
         this.boardGame = new ArrayList<Cell>();
-        this.whichArray = 1;
     }
 
     // Methods
 
     // Update the values of boardgame List with objects
-    public void addItemInBoardGame(int [] array, List boardGame) {
+    public void addItemInBoardGame(int [] array, List<Cell> boardGame, possibleCell typeOfCell) {
         for (int value: array) {
-            switch (whichArray) {
-                case 1: {
+            switch (typeOfCell) {
+                case Dragon: {
                     boardGame.set(value, new Dragon());
                     break;
                 }
-                case 2: {
+                case Sorcerer: {
                     boardGame.set(value, new Sorcerer());
                     break;
                 }
-                case 3: {
-                    boardGame.set(value, new Gobelin());
+                case Goblin: {
+                    boardGame.set(value, new Goblin());
                     break;
                 }
-                case 4: {
+                case Club: {
                     boardGame.set(value, new Club());
                     break;
                 }
-                case 5: {
+                case Sword: {
                     boardGame.set(value, new Sword());
                     break;
                 }
-                case 6: {
+                case Thunderbolt: {
                     boardGame.set(value, new Thunderbolt());
                     break;
                 }
-                case 7: {
+                case Fireball: {
                     boardGame.set(value, new Fireball());
                     break;
                 }
-                case 8: {
+                case StandardPotion: {
                     boardGame.set(value, new StandardPotion());
                     break;
                 }
-                case 9: {
+                case BigPotion: {
                     boardGame.set(value, new BigPotion());
                     break;
                 }
             }
         }
-        whichArray++;
     }
 
     // initialize all the cells of the list to null
-    public void initializeBoardGame() {
+    public void initializeCellsBoardGame() {
         for (int i = 0; i <= 64; i++) {
             boardGame.add(i, null);
+
         }
     }
 
+    // Enumerate possible pop in the different cells
+    public enum possibleCell {
+        Dragon, Sorcerer, Goblin, Club, Sword, Thunderbolt, Fireball, StandardPotion, BigPotion
+    }
+    // indicate for each type of cell where they will pop
     public void cellContent() {
         // I store all enemies position in a standard array
         int[] dragons = {45, 52, 56, 62};
         int[] sorcerer = {10, 20, 25, 32, 35, 36, 37, 40, 44, 47};
-        int[] gobelins = {3, 6, 9, 12, 15, 18, 21, 24, 27, 30};
+        int[] goblins = {3, 6, 9, 12, 15, 18, 21, 24, 27, 30};
         int[] clubs = {2, 11, 5, 22, 38};
         int[] swords = {19, 26, 42, 53};
         int[] thunderbolt = {1, 4, 8, 17, 23};
         int[] fireballs = {48, 49};
         int[] standardPotion = {7, 13, 31, 33, 39, 43};
         int[] bigPotion = {28, 41};
-        // addItemInBoardGame permit to set the value of the cell with the correct object
-        addItemInBoardGame(dragons, this.boardGame);
-        addItemInBoardGame(sorcerer, this.boardGame);
-        addItemInBoardGame(gobelins, this.boardGame);
-        addItemInBoardGame(clubs, this.boardGame);
-        addItemInBoardGame(swords, this.boardGame);
-        addItemInBoardGame(thunderbolt, this.boardGame);
-        addItemInBoardGame(fireballs, this.boardGame);
-        addItemInBoardGame(standardPotion, this.boardGame);
-        addItemInBoardGame(bigPotion, this.boardGame);
+
+        // addItemInBoardGame permit to set the value of the cell
+        addItemInBoardGame(dragons, this.boardGame, possibleCell.Dragon);
+        addItemInBoardGame(sorcerer, this.boardGame, possibleCell.Sorcerer);
+        addItemInBoardGame(goblins, this.boardGame, possibleCell.Goblin);
+        addItemInBoardGame(clubs, this.boardGame, possibleCell.Club);
+        addItemInBoardGame(swords, this.boardGame, possibleCell.Sword);
+        addItemInBoardGame(thunderbolt, this.boardGame, possibleCell.Thunderbolt);
+        addItemInBoardGame(fireballs, this.boardGame, possibleCell.Fireball);
+        addItemInBoardGame(standardPotion, this.boardGame, possibleCell.StandardPotion);
+        addItemInBoardGame(bigPotion, this.boardGame, possibleCell.BigPotion);
     }
+
+
 
     // Initialise the Count box to 1
     public void initializeFirstCellAndBoardGame() {
         character.setCharacterPosition(1);
         System.out.println("Vous commencez sur la case : " + character.getCharacterPosition());
         // Set all indexes of the boardgame to null
-        initializeBoardGame();
+        initializeCellsBoardGame();
         cellContent();
     }
 
